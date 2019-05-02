6d11cc7a5e009594e838c9f885fd639648f5c031
amh56 <amh56@pct.edu>
2019-05-02 14:56:10 -0400

    Task class updated

--- app/src/main/java/www/pbergsproductions/taskkeeper/Task.java
+++ app/src/main/java/www/pbergsproductions/taskkeeper/Task.java
@@ -1,4 +1,69 @@
 package www.pbergsproductions.taskkeeper;
 
+import java.util.Date;
+
 public class Task {
+    private String name;
+    private Date dueDate;
+    private int priority;
+    private String description;
+
+    public Task() {
+        super();
+    }
+
+    public Task(String name, Date dueDate, int priority, String description) {
+        this.name = name;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.description = description;
+    }
+
+    public Task(String name, Date dueDate, int priority) {
+        this.name = name;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "name='" + name + '\'' +
+                ", dueDate=" + dueDate +
+                ", priority=" + priority +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
